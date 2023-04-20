d1843b288a2c49e1ee7ea4063e4899b103825b36
Ruslan <rrusanovich22gmail.com>
2023-04-20 17:15:06 +0300

    added HTTP exceptions

--- routes/twitch_routes.py
+++ routes/twitch_routes.py
@@ -18,9 +18,15 @@ async def all_games(token=Depends(get_token)):
 
 
 @twitch_router.get("/twitch/game_streams", response_model=Page[Stream])
-async def game_streams(game_name: str, language: str = None, token=Depends(get_token)):
+async def game_streams(game_name: str = None, language: str = None, token=Depends(get_token)):
 
     """Streams of a game by exact game-name. Can filter by language of a streamer."""
+
+    if not game_name:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Query parameter 'game_name' required",
+        )
 
     game_id = get_game_id(game_name, token)
     streams = get_game_streams(game_id, token, language)
@@ -28,18 +34,30 @@ async def game_streams(game_name: str, language: str = None, token=Depends(get_t
 
 
 @twitch_router.get("/twitch/search_games", response_model=Page[SearchResult])
-async def search_twitch_game(query: str, token=Depends(get_token)):
+async def search_twitch_game(query: str = None, token=Depends(get_token)):
 
     """Search games and their id`s by inexact name."""
+
+    if not query:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Query parameter required",
+        )
 
     result = search_game(token, query)
     return paginate(result)
 
 
 @twitch_router.get("/twitch/search_channels", response_model=Page[Channel])
-async def search_twitch_channel(query: str, token=Depends(get_token)):
+async def search_twitch_channel(query: str = None, token=Depends(get_token)):
 
     """Search streams with query value in game name or stream name"""
+
+    if not query:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Query parameter required",
+        )
 
     result = search_channels(token, query)
     return paginate(result)
@@ -50,6 +68,13 @@ async def search_twitch_user(user_id: int = None, user_login: str = None, token=
 
     """Get info of a single user."""
 
-    user = get_user(token, user_id, user_login)[0]
+    if user_id or user_login:
+        user = get_user(token, user_id, user_login)[0]
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Provide either 'user_id' or 'user_login' parameter",
+        )
+
     return user
 
